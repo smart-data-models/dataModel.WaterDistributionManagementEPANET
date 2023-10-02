895b5e1aeca2f2592cd2424520cbb506c4ea204d
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:28:34 +0200

    test of SQL export

--- WaterNetwork/schema.sql
+++ WaterNetwork/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model WaterNetwork of the subject dataModel.WaterDistributionManagementEPANET for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model WaterNetwork of the subject dataModel.WaterDistributionManagementEPANET for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE WaterNetwork_type AS ENUM ('WaterNetwork');
-CREATE TABLE WaterNetwork (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasSubNetwork json, id text, isComposedOf json, location json, name text, owner json, seeAlso json, source text, type WaterNetwork_type);
+CREATE TABLE WaterNetwork (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasSubNetwork JSON, isComposedOf JSON, name TEXT, owner JSON, source TEXT, type WaterNetwork_type);
