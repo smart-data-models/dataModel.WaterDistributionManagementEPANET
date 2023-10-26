ebcf297ae8f7416499342b7796ecc6071e92d27e
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:52:35 +0200

    test of SQL export

--- Curve/schema.sql
+++ Curve/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Curve of the subject dataModel.WaterDistributionManagementEPANET for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE curveType_type AS ENUM ('FLOW-HEAD','FLOW-EFFICIENCY','FLOW-HEADLOSS','LEVEL-VOLUME');CREATE TYPE Curve_type AS ENUM ('Curve');
-CREATE TABLE Curve (address JSON, alternateName TEXT, areaServed TEXT, curveType curveType_type, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, tag TEXT, type Curve_type, xData JSON, yData JSON);
+CREATE TABLE Curve (address JSON, alternateName TEXT, areaServed TEXT, curveType curveType_type, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, tag TEXT, type Curve_type, xData JSON, yData JSON);
