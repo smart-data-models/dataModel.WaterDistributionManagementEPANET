42cd1fad7093c52392482f13df16cb0c1d9c9155
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:52:42 +0200

    test of SQL export

--- Pattern/schema.sql
+++ Pattern/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Pattern of the subject dataModel.WaterDistributionManagementEPANET for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Pattern_type AS ENUM ('Pattern');
-CREATE TABLE Pattern (alternateName TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, multipliers JSON, name TEXT, owner JSON, source TEXT, startTime TIMESTAMP, tag TEXT, timeStep NUMERIC, type Pattern_type);
+CREATE TABLE Pattern (alternateName TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, multipliers JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, startTime TIMESTAMP, tag TEXT, timeStep NUMERIC, type Pattern_type);
