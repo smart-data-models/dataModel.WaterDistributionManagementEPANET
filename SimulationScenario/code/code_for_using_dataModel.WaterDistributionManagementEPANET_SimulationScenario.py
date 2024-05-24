131b5f26bfd4d1973d14782c45958d4d5c91a193
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:18:57 +0200

    created example of code

--- SimulationScenario/code/code_for_using_dataModel.WaterDistributionManagementEPANET_SimulationScenario.py
+++ SimulationScenario/code/code_for_using_dataModel.WaterDistributionManagementEPANET_SimulationScenario.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "SimulationScenario"
 subject = "dataModel.WaterDistributionManagementEPANET"
-accuracy = {'type': 'Property', 'value': 0.001, 'unitCode': 'C62'}
+accuracy = 0.001
 attribute = "accuracy"
 value = accuracy
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-bulkOrder = {'type': 'Property', 'value': 1, 'unitCode': 'C62'}
+bulkOrder = 1
 attribute = "bulkOrder"
 value = bulkOrder
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-checkFrequency = {'type': 'Property', 'value': 2, 'unitCode': 'C62'}
+checkFrequency = 2
 attribute = "checkFrequency"
 value = checkFrequency
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-chemicalName = "{'type': 'Property', 'value': 'Chlorine'}"
+chemicalName = "Chlorine"
 attribute = "chemicalName"
 value = chemicalName
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
